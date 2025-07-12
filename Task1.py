2a048a7bd71ffc0f2cd6fe6a5a6f2a3be32a374f
897ARuN <897arun@gmail.com>
2025-07-12 13:13:38 +0530

    months print

---
+++ Task1.py
@@ -0,0 +1,24 @@
+ # *****elif***** 11/6(task1)
+
+print("1 for january ")
+print("2 for february")
+print("3 for march")
+print("4 for april ")
+print("5 for  may ")
+print("6 for june ")
+
+a = int(input("choose your number between 1-6"))
+
+if a==1:
+    print("January")
+elif a==2:
+    print("february")
+elif a==3:
+    print("march")
+elif a==4:
+    print("april")
+elif a==5:
+    print("may")
+elif a==6:
+    print("june")
+else: print("invalid number")
